41cb96b6bdc2df4046218577864f99f0432e6aa9
kevinjuliow <kevjuliow@gmail.com>
2023-05-18 18:23:00 +0700

    Commit

--- src/Pages/HomePages/MovieRecommendation2/MovieRecommendation2LeftContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const MovieRecommendation2LeftContainer = () => {
-  return (
-    <div>MovieRecommendation2LeftContainer</div>
-  )
-}
--- src/Pages/HomePages/MovieRecommendation2/MovieRecommendation2RightContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const MovieRecommendation2RightContainer = () => {
-  return (
-    <div>MovieRecommendation2RightContainer</div>
-  )
-}
